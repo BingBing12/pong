0e9d9840539816db2bf95c1a602bd7b98dda8f67
Robin Tainton <robintainton0509@gmail.com>
2021-12-17 21:16:50 +0200

    implementing ball and ball collision with walls and paddles, and ball bounce physics

---
+++ ball.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+import time
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+
+    def move(self):
+        self.forward(2)
+        time.sleep(0.001)
+
+    def collision(self):
+        angle = -self.heading()
+        self.setheading(angle)
+
+    def pad_collision(self):
+        angle = -self.heading() + 180
+        self.setheading(angle)
+        self.move()
+
--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+
 
 p1 = Paddle((-380, 0))
-p2 = Paddle((380, 0))
+p2 = Paddle((370, 0))
+
 
 screen = Screen()
-
+screen.tracer(0)
+ball = Ball()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 
-
+screen.listen()
 screen.onkey(p1.go_up, "w")
 screen.onkey(p1.go_down, "s")
 screen.onkey(p2.go_up, "Up")
 screen.onkey(p2.go_down, "Down")
-screen.listen()
+
+game_on = True
+ball.setheading(20)
+while game_on:
+    screen.update()
+    if ball.distance(p1) < 22 or ball.distance(p2) < 22:
+        ball.pad_collision()
+    if ball.ycor() > 285 or ball.ycor() < -280:
+        ball.collision()
+    ball.move()
+
 screen.exitonclick()
